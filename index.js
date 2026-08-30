58a59f698cea502193233819e8a6b25c5fc78522
Himu <himangshunaskar82@gmail.com>
2026-08-30 15:48:19 +0530

    1st entry

---
+++ index.js
@@ -0,0 +1,6 @@
+let p = document.createElement('p');
+p.textContent = 'Hello, World!';
+let l = document.querySelector('ol');
+let listItem = document.createElement('li');
+listItem.textContent = 'Raj';
+l.appendChild(listItem);
